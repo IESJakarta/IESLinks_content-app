a8e36206e1f139031ef71a49d5a0741e236c1a81
Michael S. Rody <mike.rody@iesjakarta.org>
2023-12-02 03:47:52 +0700

    Updated Calendars

--- components/content/CombinedCalendarView.vue
+++ components/content/CombinedCalendarView.vue
@@ -1,27 +1,18 @@
 <template>
-    <object
-      data="https://calendar.google.com/calendar/embed?height=600
+    <object data="https://calendar.google.com/calendar/embed?height=600
 &wkst=1
 &bgcolor=%23ffffff
 &ctz=Asia%2FJakarta
-&title=Combined%20IES%20Calendar
-&showTabs=1
-&showPrint=1
-&showNav=1
+&src=Y19jMzZiY2UwMDRjMThhZjA3NWZmN2NjMGNmOTk0MjljNjFjNjQ5ZmYyYjhiYzVmM2YzY2QzYTA0MmY5ZjExY2QxQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20
+&src=Y18wOWEzODVkNThlOTY3MjA0Y2NjNWE4MjZkMWJlMTZkNzg0ZWU1NGUyMjdiMmUxNzkzNzM1NGJjMzZhMDBhZDgwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20
 &src=c29waGllQGllc2pha2FydGEub3Jn
-&src=aWVzamFrYXJ0YS5vcmdfYzRnc2xmbWRxZmM2aGhrZjk2dHNjMXU1Y2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ
-&src=aWVzZW5jbnRyQGllc2pha2FydGEub3Jn
-&src=aW50ZXJuc0BpZXNqYWthcnRhLm9yZw
 &src=dGVlbnNAaWVzamFrYXJ0YS5vcmc
-&color=%237CB342
-&color=%23D81B60S
-&color=%23F6BF26
-&color=%23B39DDB
-&color=%234285F4"
-      style="border-width: 0"
-      width="800"
-      height="600"
-      frameborder="0"
-      scrolling="no"
-    ></object>
+&src=aW50ZXJuc0BpZXNqYWthcnRhLm9yZw
+&color=%23E4C441
+&color=%23F09300
+&color=%23795548
+&color=%234285F4
+&color=%23B39DDB" 
+style="border-width:0" width="800" height="600" frameborder="0" scrolling="no">
+    </object>
 </template>
