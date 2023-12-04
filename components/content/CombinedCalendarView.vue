b7b996d6d13b504d470f83fe84c944967c7316c0
Michael S. Rody <mike.rody@iesjakarta.org>
2023-12-04 18:27:08 +0700

    Updated Calendars

--- components/content/CombinedCalendarView.vue
+++ components/content/CombinedCalendarView.vue
@@ -1,18 +1,24 @@
 <template>
-    <object data="https://calendar.google.com/calendar/embed?height=600
-&wkst=1
-&bgcolor=%23ffffff
-&ctz=Asia%2FJakarta
-&src=Y19jMzZiY2UwMDRjMThhZjA3NWZmN2NjMGNmOTk0MjljNjFjNjQ5ZmYyYjhiYzVmM2YzY2QzYTA0MmY5ZjExY2QxQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20
-&src=Y18wOWEzODVkNThlOTY3MjA0Y2NjNWE4MjZkMWJlMTZkNzg0ZWU1NGUyMjdiMmUxNzkzNzM1NGJjMzZhMDBhZDgwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20
-&src=c29waGllQGllc2pha2FydGEub3Jn
-&src=dGVlbnNAaWVzamFrYXJ0YS5vcmc
-&src=aW50ZXJuc0BpZXNqYWthcnRhLm9yZw
-&color=%23E4C441
-&color=%23F09300
-&color=%23795548
-&color=%234285F4
-&color=%23B39DDB" 
-style="border-width:0" width="800" height="600" frameborder="0" scrolling="no">
-    </object>
+	<object data="https://calendar.google.com/calendar/embed?height=600
+	&wkst=1
+	&bgcolor=%23ffffff
+	&ctz=Asia%2FJakarta
+	&src=Y19jMzZiY2UwMDRjMThhZjA3NWZmN2NjMGNmOTk0MjljNjFjNjQ5ZmYyYjhiYzVmM2YzY2QzYTA0MmY5ZjExY2QxQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20
+	&src=Y19kNzUyZWJhNDIyZGIzYzNhZjM0OGNjMTRkM2RkNzhiOTM2MjAwOWQ4NjYwMmQwODZkMmVkMjk4YWUwNDRkNjA0QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20
+	&src=aWVzamFrYXJ0YS5vcmdfYzRnc2xmbWRxZmM2aGhrZjk2dHNjMXU1Y2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ
+	&src=Y18wOWEzODVkNThlOTY3MjA0Y2NjNWE4MjZkMWJlMTZkNzg0ZWU1NGUyMjdiMmUxNzkzNzM1NGJjMzZhMDBhZDgwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20
+	&src=Y182YmZmYWQ5ZTNjNTFiMjkwZWFlYTEzMzI3M2I1NTUzMDM0OTg5NzNlOGY1NjdiNjQ2NDBkNGRlNDFmMGZhNDBhQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20
+	&src=dGVlbnNAaWVzamFrYXJ0YS5vcmc
+	&color=%23E4C441
+	&color=%239E69AF
+	&color=%23D81B60
+	&color=%233F51B5
+	&color=%23C0CA33
+	&color=%234285F4" 
+	style="border:solid 1px #777" 
+	width="800" 
+	height="600" 
+	frameborder="0" 
+	scrolling="no">
+	</object>
 </template>
