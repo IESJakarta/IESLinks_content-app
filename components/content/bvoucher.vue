2ac14b13a0fa76f761563f8fc15d897ec144c39f
msrody <mike.rody@gmail.com>
2024-03-06 11:26:30 +0700

    f

--- components/content/bvoucher.vue
+++ components/content/bvoucher.vue
@@ -670,3 +670,81 @@
       border-color: transparent;
       }
 </style>
+
+<script>
+const aTens = [ "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+const aOnes = [ "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+  "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", 
+  "Nineteen" ];
+function ConvertToHundreds(num)
+{
+   var cNum, nNum;
+   var cWords = "";
+   num %= 1000;
+   if (num > 99) {
+      /* Hundreds. */
+      cNum = String(num);
+      nNum = Number(cNum.charAt(0));
+      cWords += aOnes[nNum] + " Hundred";
+      num %= 100;
+      if (num > 0)
+         cWords += " and "
+   }
+
+   if (num > 19) {
+      /* Tens. */
+      cNum = String(num);
+      nNum = Number(cNum.charAt(0));
+      cWords += aTens[nNum - 2];
+      num %= 10;
+      if (num > 0)
+         cWords += "-";
+   }
+   if (num > 0) {
+      /* Ones and teens. */
+      nNum = Math.floor(num);
+      cWords += aOnes[nNum];
+   }
+   return cWords;
+}
+
+
+function ConvertToWords(num,cur)
+{
+   var aUnits = [ "Thousand", "Million", "Billion", "Trillion", "Quadrillion" ];
+   var cWords = (num >= 1 && num < 2) ? " " : " and ";
+   var nLeft = Math.floor(num);
+   for (var i = 0; nLeft > 0; i++) { 
+       if (nLeft % 1000 > 0) {
+          if (i != 0)
+             cWords = ConvertToHundreds(nLeft) + " " + aUnits[i - 1] + " " + cWords;
+          else
+             cWords = ConvertToHundreds(nLeft) + " " + cWords;
+       }
+       nLeft = Math.floor(nLeft / 1000);
+   }
+   num = Math.round(num * 100) % 100;
+   if (num > 0)
+      cWords += ConvertToHundreds(num) + " /100 " + (cur);
+   else
+      cWords += "00/100 " + (cur);
+   return cWords;
+}
+
+
+function ammountinput() {
+   let f = document.getElementById('AmountInput').value; 
+   
+   if (document.getElementsByName("currency").value = "IDR") {
+   var cur = "Rupiah";
+   } else if (document.getElementById("currency").value = "USD") {
+   var cur = "US Dollars";
+   } else {
+      var cur = "PLEASE SELECT A CURRENCY";
+   }
+   value = ConvertToWords(f,cur);
+   document.getElementById("SayInput").innerHTML = value;
+
+}
+</script>
+
