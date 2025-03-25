0ee97bf530da250a93b3e2c5bc093e813610e8a1
mike.rody <mike.rody@iesjakarta.org>
2025-03-25 05:54:05 +0000

    added adminIPMetTest.vue

---
+++ components/content/adminIPMetTest.vue
@@ -0,0 +1,27 @@
+
+<script setup>
+const url = `https://script.google.com/macros/s/AKfycbwO_yu99EqVewc0O4wCgKRKZkpa9YIdOqFrK1ZkbiYPWZwj_4BbYs9G-rZ4H8mS9vmDXA/exec?tab=IP_Headcounts`
+var { data: hcount } = await useFetch(url)
+</script>
+
+<template>
+  <div class=topgrid style="text-align: left">
+    <p></p>
+    <span style="text-align: center">
+      <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
+      <h3 style="margin-bottom: 0;">Weekend of {{ count["Weekend Date"] }}</h3>
+    </span>
+    <p></p>
+    <p></p>
+
+            {{ hcount }}
+
+  </div>
+</template>
+
+<style scoped>
+li {
+  line-height: .9em;
+  margin-bottom: 20px;
+}
+</style>
