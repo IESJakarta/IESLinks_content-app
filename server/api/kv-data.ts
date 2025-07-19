3fd12b4394dacf308d6734313afef027c9fb2028
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-18 17:53:47 -0700

    d

--- server/api/kv-data.ts
+++ server/api/kv-data.ts
@@ -3,7 +3,7 @@ export default defineEventHandler(async (event) => {
   try {
     // Access your KV binding through event.context.cloudflare.env
     // Replace 'MY_KV_BINDING' with the name you gave your KV binding in the Cloudflare dashboard.
-    const myKVBinding = event.context.cloudflare.env.MY_KV_BINDING; 
+    const myKVBinding = event.context.cloudflare.env.metricskv; 
 
     // Retrieve data from KV
     const data = await myKVBinding.get("ThisWeek"); // Replace "your-key" with the key you want to retrieve
