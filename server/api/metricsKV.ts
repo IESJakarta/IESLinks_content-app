685b593442b1a0527a819e48a9e9470ed6aa7be6
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-25 16:28:59 -0700

    f

--- server/api/metricsKV.ts
+++ server/api/metricsKV.ts
@@ -1,11 +1,5 @@
-import { defineEventHandler } from 'h3';
-
-interface Env {
-  MY_KV_NAMESPACE: metricskv;
-};
-
-export default defineEventHandler(async (event) => {
-  const { MY_KV_NAMESPACE } = event.context.cloudflare.env as Env;
-  const myValue = await MY_KV_NAMESPACE.get('ThisWeek');
-  return { data: myValue };
-});
+    // Example in a server route or API endpoint
+    export default defineEventHandler(async (event) => {
+      const value = await event.context.env.metricskv.get("ThisWeek");
+      return { data: value };
+    });
