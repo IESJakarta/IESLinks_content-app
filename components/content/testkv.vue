1d70f6c699c16db20f3df832af6b208f68971e87
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-25 14:14:17 -0700

    i

--- components/content/testkv.vue
+++ components/content/testkv.vue
@@ -1,13 +1,15 @@
     <script setup>
     const { data } = await useFetch('/api/metricsKV')
+    const outstring = JSON.parse(data.data)
     </script>
 
     <template>
       <div>
-        <p>Data from KV: {{ data }}</p>
+        <p>outstring.ThisWeek: {{ outstring.ThisWeek }}
+        <p>Data from KV: {{ data.data }}</p>
         <p>Using a for in pattern</p>
             <ul>
-            <li v-for="(x, key) in data" style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
+            <li v-for="(x, key) in data.data" style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
                 <table style=" margin: 0px auto;">
                 <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
                     <td> {{ loc }}: </td>
