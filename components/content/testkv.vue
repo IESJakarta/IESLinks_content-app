47719a489bd6ce4780e9a9425c042d76b44fc665
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-25 20:20:35 -0700

    j

--- components/content/testkv.vue
+++ components/content/testkv.vue
@@ -4,10 +4,10 @@
 
     <template>
       <div>
-        <p>Data from KV: {{ data.count }}</p>
+        <p>Data from KV: {{ data.value }}</p>
         <p>Using a for in pattern</p>
             <ul>
-      <li v-for="(x, key) in count.servicedata" style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
+      <li v-for="(x, key) in value" style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
         <table style=" margin: 0px auto;">
           <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
             <td> {{ loc }}: </td>
