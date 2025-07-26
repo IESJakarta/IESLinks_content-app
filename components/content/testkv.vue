7963cbf9e689cc35be0fd510444687ad5f43c51f
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-25 20:48:11 -0700

    work

--- components/content/testkv.vue
+++ components/content/testkv.vue
@@ -6,6 +6,35 @@
     <h1 v-if="data">{{ data.data }}</h1>
     <p v-if="pending">Loading...</p>
     <p v-if="error">Error: {{ error.message }}</p>
+
+
+
+  <div class=topgrid style="text-align: left">
+    <p></p>
+    <span style="text-align: center">
+      <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
+      <h3 style="margin-bottom: 0;">Weekend of {{ data.data.IPdata.["Weekend Date"] }}</h3>
+    </span>
+    <p></p>
+    <p></p>
+    <ul>
+      <li v-for="(x, key) in data.data.IPdata.servicedata" style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
+        <table style=" margin: 0px auto;">
+          <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
+            <td> {{ loc }}: </td>
+            <td style="text-align: right; padding-left: 1em;"> {{ y }}</td>
+          </tr>
+        </table>
+      </li>
+    </ul>
+  </div>
+
+<style scoped>
+li {
+  line-height: .9em;
+  margin-bottom: 20px;
+}
+</style>
   </div>
 </template>
 
