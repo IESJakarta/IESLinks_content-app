89c55d490629f20179cfb0579e41fc6f9add2755
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-27 15:40:54 -0700

    fghjkl

--- components/content/testkvThisWeek.vue
+++ components/content/testkvThisWeek.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     This Weekend Date ->>>>  
-    <h1 v-if="data">{{ data.ThisWeek["Weekend Date"] }}</h1>
+    <h1 v-if="data">{{ data.ThisWeek['Weekend Date'] }}</h1>
     <p v-if="pending">Loading...</p>
     <p v-if="error">Error: {{ error.message }}</p>
 
