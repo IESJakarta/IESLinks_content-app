02a8168d762dd7e49c53dfb3abdfdbfb1f5b260c
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-27 13:59:18 -0700

    iuy

--- components/content/testkvThisWeek.vue
+++ components/content/testkvThisWeek.vue
@@ -32,7 +32,7 @@
 <script setup lang="ts">
 import { useFetch } from '#app'; // Nuxt composable for data fetching
 
-const { data: ThisWeek, pending, error } = await useFetch('/api/metricsKV'); //
+const { ThisWeek, pending, error } = await useFetch('/api/metricsKV'); //
 
 
 </script>
