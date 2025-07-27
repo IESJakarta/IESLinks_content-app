a78e7376713e662a96d72b90152ca2c55550c794
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-27 15:24:11 -0700

    JSON.parse as ithe date come in from the kv

--- components/content/testkvIPdata.vue
+++ components/content/testkvIPdata.vue
@@ -5,7 +5,7 @@
     ThisWeek
  
     IP weekend date >>>>->
-    <h1 v-if="data">{{ data.IPdata["Weekend Date"] }}</h1>
+    <h1 v-if="data">{{ data.IPdata['Weekend Date'] }}</h1>
     <p v-if="pending">Loading...</p>
     <p v-if="error">Error: {{ error.message }}</p>
 
