56669ec8f5d5b5d859908fcb4e26148c2317b3c1
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-27 18:22:28 -0700

    ssssss

--- components/content/testkvThisWeek.vue
+++ components/content/testkvThisWeek.vue
@@ -29,14 +29,37 @@
     <div class=topgrid style="text-align: left">
       <p></p>
       <span style="text-align: center">
-        <h3 style="margin-bottom: .2em;">IES servicedata - data.ThisWeek.IPdata.servicedata </h3>
-        <h3 style="margin-bottom: 0;">data --VV--</h3><p>{{ data.ThisWeek.IPdata.servicedata }}</p><h3 style="margin-bottom: 0;"> ^^^ </h3>
+        <h3 style="margin-bottom: .2em;">IES servicedata - "data.ThisWeek.IPdata.servicedata" </h3>
+        <h3 style="margin-bottom: 0;">data --VV--</h3>
+        <p>{{ data.ThisWeek.IPdata.servicedata }}</p>
+        <h3 style="margin-bottom: 0;"> ^^^ </h3>
 
 
       </span>
       <p></p>
       <p></p>
 
+
+    </div>
+    <div class=topgrid style="text-align: left">
+      <p></p>
+      <span style="text-align: center">
+        <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
+        <h3 v-if="data" style="margin-bottom: 0;">Weekend of {{ data.ThisWeek.IPdata['Weekend Date'] }}</h3>
+      </span>
+      <p></p>
+      <p></p>
+      <ul>
+        <li v-for="(x, key) in data.ThisWeek.IPdata.servicedata"
+          style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
+          <table style=" margin: 0px auto;">
+            <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
+              <td> {{ loc }}: </td>
+              <td style="text-align: right; padding-left: 1em;"> {{ y }}</td>
+            </tr>
+          </table>
+        </li>
+      </ul>
     </div>
 
   </div>
