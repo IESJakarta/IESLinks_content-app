c25650cbb5fa6b0db705ac653182b69be887ff10
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-27 22:35:51 -0700

    just output

--- components/content/testkvThisWeek.vue
+++ components/content/testkvThisWeek.vue
@@ -2,43 +2,10 @@
 
 <template>
   <div>
-    This Weekend Date ->>>>
     <h1 v-if="data">{{ data.ThisWeek.IPdata['Weekend Date'] }}</h1>
     <p v-if="pending">Loading...</p>
     <p v-if="error">Error: {{ error.message }}</p>
 
-    <div>
-      <br><br>
-      <p>Just the data</p>
-      <p>{{ data }}</p>
-    </div>
-
-    <div>
-      <br><br>
-      <p>this Week - data.ThisWeek</p>
-      <p>{{ data.ThisWeek }}</p>
-    </div>
-
-    <div>
-      <br><br>
-      <p>IPdata - data.ThisWeek.IPdata</p>
-      <p>{{ data.ThisWeek.IPdata }}</p>
-    </div>
-
-
-    <div class=topgrid style="text-align: left">
-      <p></p>
-      <span style="text-align: center">
-        <h3 style="margin-bottom: .2em;">IES servicedata - "data.ThisWeek.IPdata.servicedata" </h3>
-        <h3 style="margin-bottom: 0;">data --VV--</h3>
-        <p>{{ data.ThisWeek.IPdata.servicedata }}</p>
-        <h3 style="margin-bottom: 0;"> ^^^ </h3>
-      </span>
-
-      <p></p>
-      <p></p>
-
-    </div>
     <div class=topgrid style="text-align: left">
       <p></p>
       <span style="text-align: center">
