68d73387105c44698c7efce5fcc0c3600649b926
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-27 19:40:04 -0700

    metricsInPrThisWeekKV.

--- components/content/metricsInPrThisWeekKV.vue
+++ components/content/metricsInPrThisWeekKV.vue
@@ -1,25 +1,31 @@
 <!-- components/MyComponent.vue -->
 
 <template>
-  <div class=topgrid style="text-align: left">
-    <p></p>
-    <span style="text-align: center">
-      <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
-      <h3 v-if="data" style="margin-bottom: 0;">Weekend of {{ data.ThisWeek.IPdata['Weekend Date'] }}</h3>
-    </span>
-    <p></p>
-    <p></p>
-    <ul>
-      <li v-for="(x, key) in data.ThisWeek.IPdata.servicedata"
-        style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
-        <table style=" margin: 0px auto;">
-          <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
-            <td> {{ loc }}: </td>
-            <td style="text-align: right; padding-left: 1em;"> {{ y }}</td>
-          </tr>
-        </table>
-      </li>
-    </ul>
+  <div>
+    <h1 v-if="data">{{ data.ThisWeek.IPdata['Weekend Date'] }}</h1>
+    <p v-if="pending">Loading...</p>
+    <p v-if="error">Error: {{ error.message }}</p>
+    
+    <div class=topgrid style="text-align: left">
+      <p></p>
+      <span style="text-align: center">
+        <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
+        <h3 v-if="data" style="margin-bottom: 0;">Weekend of {{ data.ThisWeek.IPdata['Weekend Date'] }}</h3>
+      </span>
+      <p></p>
+      <p></p>
+      <ul>
+        <li v-for="(x, key) in data.ThisWeek.IPdata.servicedata"
+          style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
+          <table style=" margin: 0px auto;">
+            <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
+              <td> {{ loc }}: </td>
+              <td style="text-align: right; padding-left: 1em;"> {{ y }}</td>
+            </tr>
+          </table>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -33,7 +39,6 @@ li {
 <script setup lang="ts">
 import { useFetch } from '#app'; // Nuxt composable for data fetching
 
-const { data, pending, error } = await useFetch('/api/metricsKV'); //
-
+const { data, pending, error } = await useFetch('/api/metricsKV'); 
 
 </script>
