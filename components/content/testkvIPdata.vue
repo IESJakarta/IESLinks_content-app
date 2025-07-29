602f35bff7aaca1cf8402be6d1ff59ea959532eb
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-29 11:54:06 -0700

    /api/metricsKVIPdata

--- components/content/testkvIPdata.vue
+++ components/content/testkvIPdata.vue
@@ -2,26 +2,35 @@
 
 <template>
   <div>
-    ThisWeek
+    testkvIPdata.vue component getting data from /api/metricsKVIPdata
 
     IP weekend date >>>>->
-    <h1 v-if="data">{{ data.IPdata['Weekend Date'] }}</h1>
-    <p v-if="pending">Loading...</p>
-    <p v-if="error">Error: {{ error.message }}</p>
 
+      <h1 v-if="data">{{ data.IPdata['Weekend Date'] }}</h1>
+      <p v-if="pending">Loading...</p>
+      <p v-if="error">Error: {{ error.message }}</p>
 
-
-    <div class=topgrid style="text-align: left">
-      <p></p>
-      <span style="text-align: center">
-        <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
-        <h3 v-for="d in data.IPdata.servicedata" style="margin-bottom: 0;">data {{ d }}</h3>
-
-
-      </span>
-      <p></p>
-      <p></p>
-
+      <div class=topgrid style="text-align: left">
+        <p>
+          <span style="text-align: center">
+            <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
+            <h3 v-if="data" style="margin-bottom: 0;">Weekend of {{ data.IPdata['Weekend Date'] }}</h3>
+          </span>
+        </p>
+        <p></p>
+        <p></p>
+        <ul>
+          <li v-for="(x, key) in data.IPdata.servicedata"
+            style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
+            <table style=" margin: 0px auto;">
+              <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
+                <td> {{ loc }}: </td>
+                <td style="text-align: right; padding-left: 1em;"> {{ y }}</td>
+              </tr>
+            </table>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
