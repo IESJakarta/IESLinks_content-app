b75df376675494578c4251abc90e2d9099caa2b4
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-29 20:01:46 -0700

    to js

--- components/content/metricsIPTotalskv.vue
+++ components/content/metricsIPTotalskv.vue
@@ -4,7 +4,7 @@
   <div>
     <div class=topgrid style="text-align: left">
 
-      <h1 v-if="data">{{ data }}</h1>
+      <h1 v-if="data">{{ data.ThisWeek }}</h1>
       <p v-if="pending">Loading...</p>
       <p v-if="error">Error: {{ error.message }}</p>
             <p> show something </p>
