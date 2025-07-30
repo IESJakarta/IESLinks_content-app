de0e2e9e599852d8256f9a01683c63cec6ea77be
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-29 22:48:31 -0700

    metricsInPerThisWeekkv.vue format

--- components/content/metricsInPrThisWeekkv.vue
+++ components/content/metricsInPrThisWeekkv.vue
@@ -1,29 +1,30 @@
 <!-- components/MyComponent.vue -->
 
 <template>
-  <div>
-      <div class=topgrid style="text-align: left">
-        <p>
-          <span style="text-align: center">
-            <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
-            <h3 v-if="data" style="margin-bottom: 0;">Weekend of {{ data.IPdata['Weekend Date'] }}</h3>
-          </span>
-        </p>
-        <p></p>
-        <p></p>
-        <ul>
-          <li v-for="(x, key) in data.IPdata.servicedata"
-            style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
-            <table style=" margin: 0px auto;">
-              <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
-                <td> {{ loc }}: </td>
-                <td style="text-align: right; padding-left: 1em;"> {{ y }}</td>
-              </tr>
-            </table>
-          </li>
-        </ul>
-      </div>
+  <div class=topgrid style="text-align: left">
+    <div v-if="data">
+      <p></p>
+      <span style="text-align: center">
+        <h3 style="margin-bottom: .2em;">IES Service Attendance</h3>
+        <h3 style="margin-bottom: 0;">Weekend of {{ data.IPdata['Weekend Date'] }}</h3>
+      </span>
+      <p></p>
+      <p></p>
+      <ul>
+        <li v-for="(x, key) in data.IPdata.servicedata"
+          style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">{{ key }}
+          <table style=" margin: 0px auto;">
+            <tr v-for="(y, loc) in x" style="text-align: right; line-height: 1.2em;">
+              <td> {{ loc }}: </td>
+              <td style="text-align: right; padding-left: 1em;"> {{ y }}</td>
+            </tr>
+          </table>
+        </li>
+      </ul>
     </div>
+    <p v-if="pending">Loading...</p>
+    <p v-if="error">Error: {{ error.message }}</p>
+  </div>
 </template>
 
 <style scoped>
