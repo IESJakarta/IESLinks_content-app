472235372326e0346043fd563a288e0cff70d73e
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-29 20:50:43 -0700

    testkvIPdata.vue

--- components/content/testkvIPdata.vue
+++ components/content/testkvIPdata.vue
@@ -2,10 +2,6 @@
 
 <template>
   <div>
-    testkvIPdata.vue component getting data from /api/metricsKVIPdata
-
-    IP weekend date >>>>->
-
       <h1 v-if="data">{{ data.IPdata['Weekend Date'] }}</h1>
       <p v-if="pending">Loading...</p>
       <p v-if="error">Error: {{ error.message }}</p>
@@ -41,11 +37,7 @@ li {
 }
 </style>
 
-
 <script setup lang="ts">
 import { useFetch } from '#app'; // Nuxt composable for data fetching
-
 const { data, pending, error } = await useFetch('/api/metricsKVIPdata'); //
-
-
 </script>
