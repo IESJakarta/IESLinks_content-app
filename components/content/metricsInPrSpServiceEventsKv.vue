292091e8c732f8a95ac535a21363d749b41b0742
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-30 20:12:57 -0700

    Dev kv (#2)
    * testkv.vue
    
    * try 3000
    
    * servicedata
    
    * x
    
    * y
    
    * for
    
    * s
    
    * x
    
    * i
    
    * c
    
    * h
    
    * j
    
    * r
    
    * ls
    
    * d
    
    * f
    
    * m
    
    * All new approach
    
    * v
    
    * u
    
    * r
    
    * d
    
    * s
    
    * ;
    
    * lasjdfl
    
    * f
    
    * content
    
    * DB
    
    * i
    
    * f
    
    * d
    
    * j
    
    * d
    
    * d
    
    * fghjkl
    
    * metricsKV
    
    * metricskv
    
    * work
    
    * s
    
    * x
    
    * ThisWeek
    
    * lkjh
    
    * s
    
    * sdf
    
    * o
    
    * thisweek parsed
    
    * rtyuk
    
    * thisWeekData
    
    * xdftyjhgf
    
    * dfgh
    
    * oiuh
    
    * c
    
    * d
    
    * IPdata
    
    * d
    
    * s
    
    * s
    
    * two
    
    * gjkl
    
    * testkvThisWeek
    
    * fghjkl;
    
    * not data pending
    
    * rrr
    
    * d
    
    * iuy
    
    * ffff
    
    * ss
    
    * kjdf
    
    * data.ThisWeek
    
    * servicedata
    
    * JSON.parse as ithe date come in from the kv
    
    * fghjkl
    
    * dfghj
    
    * xc
    
    * data.ThisWeek.IPdata.servicedata
    
    * metricsInPrThisWeek-KV
    
    * ss
    
    * ljhgf
    
    * more
    
    * s
    
    * k
    
    * k
    
    * oiuyt
    
    * d
    
    * ssssss
    
    * metricsInPrThisWeekKV
    
    * laskjdf;lj
    
    * ;laskdjf
    
    * wer
    
    * metricsInPrThisWeekKV.
    
    * g
    
    * '#imports'
    
    * oih
    
    * jhg
    
    * #app
    
    * oiuyhm
    
    * hg
    
    * just output
    
    * oiuy
    
    * lky
    
    * kjh
    
    * testkvIPdata
    
    * /api/metricsKVIPdata
    
    * mnbf
    
    * IP
    
    * metricsInPrThisWeekKV
    
    * renamed with kv not KV
    
    * adding OLdata to IESMetric page
    
    * f
    
    * sdrtgh
    
    * OLdata from kv
    
    * v
    
    * cvbn
    
    * anothre
    
    * metricsTotalInPrThisWeekkv
    
    * metricsTotalInPrThisWeekkv
    
    * metricsKVIPTotals
    
    * metricsIPTotalskv
    
    * metricsIPTotalskv.vue
    
    * sdsdf
    
    * Weekend Date
    
    * dfgbn
    
    * sdf
    
    * dfghjk
    
    * vb
    
    * d
    
    * dfghjk
    
    * sdfghjkl;
    
    * metricsInPrThisWeekkv
    
    * testkvThisWeek
    
    * d
    
    * ghjk
    
    * metricsIPTotalskv data show
    
    * to js
    
    * dfghjkl
    
    * metricsIPTotalskv
    
    * sdfghjk
    
    * testkvIPdata.vue
    
    * mbvc
    
    * iptotals
    
    * dfgnm
    
    * kjhg
    
    * j
    
    * s
    
    * lkjhgf
    
    * tlkjh
    
    * metricsInPerThisWeekkv.vue format
    
    * mmm
    
    * totals in Online
    
    * OLback to normal
    
    * metricsInPrThisWeekkv.vue adding totals
    
    * var
    
    * f
    
    * wdfbn
    
    * xvbnm
    
    * cv
    
    * s
    
    * s
    
    * sdbn
    
    * emit
    
    * defineModel
    
    * fghjk
    
    * f
    
    * df
    
    * f
    
    * hj
    
    * computed totals
    
    * d
    
    * totals
    
    * metricsInPrTotalsThisWeek
    
    * asdfas
    
    * s
    
    * s
    
    * dfgh
    
    * iesmetricsthisweek.md
    
    * dfgbhnm
    
    * f
    
    * dfghm
    
    * sdfghjm
    
    * line double
    
    * dsfghjkl
    
    * ;lkjhg
    
    * IES Quick Links title
    
    * sdfgh
    
    * zxCZXcZCX
    
    * REMOVED totals components
    
    * SpSer data from kv
    
    * ## test of metricsInPrSpServiceEventsKv
    on iesmetricsthisweek.md
    
    * more try

---
+++ components/content/metricsInPrSpServiceEventsKv.vue
@@ -0,0 +1,123 @@
+<!--
+This example fetches latest Vue.js commits data from GitHub’s API and displays them as a list.
+You can switch between the two branches.
+-->
+
+<script setup>
+import { useFetch } from '#app'; // Nuxt composable for data fetching
+const { data, pending, error } = await useFetch('/api/metricsKVIPdata'); //
+var sEvents = Object.keys(data.SpServdata.value)
+
+
+let kidscount = accumulator("Kids") || 0
+let adults = accumulator("Adults") || 0
+let teens = accumulator("Teens") || 0
+let totalAttendance = (kidscount + adults + teens)
+let totals = {}
+totals.Kids = kidscount
+totals.Adults = adults
+totals.Teens = teens
+totals["Total Attendance"] = totalAttendance
+
+function accumulator(min) {
+  let accumulated = 0
+  for (var i in sEvents) {
+    let sData = Object.keys(data.SpServdata.value[sEvents[i]]["SServiceData"])
+    for (var x in sData) {
+      accumulated += data.SpServdata.value[sEvents[i]]["SServiceData"][sData[x]][min]
+      if (sData[x].split(' ')[0] === min) {
+        accumulated += data.SpServdata.value[sEvents[i]]["SServiceData"][sData[x]][min]
+      }
+    }
+  }
+  return (accumulated)
+}
+
+let tots = {}
+
+for (var i in sEvents) {
+  let sdata = Object.keys(data.SpServdata.value[sEvents[i]]["SServiceData"])
+  for (var x in sdata) {
+    let sdatakeys = Object.keys(data.SpServdata.value[sEvents[i]]["SServiceData"][sdata[x]])
+    for (var k in sdatakeys) {
+      let val = data.SpServdata.value[sEvents[i]]["SServiceData"][sdata[x]][sdatakeys[k]]
+      if (typeof val === 'number') {
+        tots[sdatakeys[k]] = val
+      }
+    }
+  }
+}
+
+let totalAtt = (Object.values(tots).reduce((a, b) => a + b, 0))
+
+
+
+</script>
+
+<template>
+  <div v-show="sEvents.length > 0">
+    <hr>
+
+    <div class=topgrid style="text-align: left; margin-top: 3em; margin-bottom: 3em;">
+
+
+      <p></p>
+      <!-- title section -->
+      <span style="text-align: center">
+        <h2>Recent Special Services</h2>
+      </span>
+      <!-- 
+      - Special Event(s)
+      -->
+      <div v-for="(value, key) in data.SpServdata">
+
+        <span style="text-align: center">
+          <h3 style="margin-bottom: .2em;">{{ value.SServiceEvent }}</h3>
+          <h3 style="margin-bottom: 0;">{{ value.SServiceDate }}</h3>
+        </span>
+        <p></p>
+        <p></p>
+        <!-- Listing Per Service attendance listing by ministry  -->
+        <div>
+          <ul>
+
+            <li v-for="(value, key, index) in value.SServiceData"
+              style="line-height: 2em; margin-left: 1.3em; margin-bottom: 1.5em;">
+              <table style=" margin: 0px auto;">
+                Service {{ index + 1 }} @ {{ value.time }}
+                <tr v-for="(x, key, index) in tots" v-show="key != 'time'"
+                  style="text-align: right; line-height: 1.2em;">
+                  <td style="text-align: right;">{{ key }}:</td>
+                  <td style="text-align: right; padding-left: 1em;">{{ x }}</td>
+                </tr>
+              </table>
+            </li>
+          </ul>
+
+        </div>
+        <!--  Totals of attendance by ministry and total attendance. -->
+        <div>
+          <p></p>
+          <table style=" margin: 0px auto;">
+            <tr style="text-align: right; line-height: 1.2em;">
+              <td style="text-align: right;">
+                <h3 style="margin-bottom: .2em;">Total Attendance:</h3>
+              </td>
+              <td style="text-align: right; padding-left: 1em;">
+                {{ totalAtt }}
+              </td>
+            </tr>
+          </table>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+li {
+  line-height: .9em;
+  margin-bottom: 20px;
+}
+</style>
