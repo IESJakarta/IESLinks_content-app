d578b84e7a3e097160e6c56c4945388c51bf4f99
IESJakarta <63225321+IESJakarta@users.noreply.github.com>
2025-07-30 22:24:59 -0700

    ggg

--- components/content/metricsInPrSpServiceEventsKv.vue
+++ components/content/metricsInPrSpServiceEventsKv.vue
@@ -6,7 +6,7 @@ You can switch between the two branches.
 <script setup>
 import { useFetch } from '#app'; // Nuxt composable for data fetching
 const { data, pending, error } = await useFetch('/api/metricsKVIPdata'); //
-var sEvents = Object.keys(data.SpServdata)
+var sEvents = Object.keys(data.SpServdata.value)
 
 
 let kidscount = accumulator("Kids") || 0
